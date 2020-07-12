4bed3ad3530c689b0c93345c8e9d6f5db454c960
IKeeny <2670268026@qq.com>
2020-07-12 22:34:25 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -3,21 +3,51 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/CSS/global.css'
-import { Form,FormItem,Button, Input,Message } from 'element-ui'
+import { Form,FormItem,Button, Input,Message,Container,
+  Aside,Main,Header,Menu,Submenu,MenuItem,MenuItemGroup,
+  Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,
+  Switch,Tooltip,Pagination,Dialog,MessageBox} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+import '@/assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
 //配置请求的根路径
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
-Vue.prototype.$https = axios
+//请求拦截器
+axios.interceptors.request.use(config=>{
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup) 
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
 
 new Vue({
   router,
